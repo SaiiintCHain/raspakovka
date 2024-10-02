e2cba31e6318ecc086380e1601ff1ca80d5d58b8
SaiiintCHain <zok-hor@mail.ru>
2024-10-02 20:53:13 +0300

    Add files via upload

---
+++ raspakouka.py
@@ -0,0 +1,11 @@
+def print_params(a, b ,c):
+    print(a, b, c)
+
+values_list = [1, 'None', True]
+values_dict = {'a': 1, 'b': 'None', 'c': True}
+print_params(*values_list)
+print_params(**values_dict)
+values_list_2 = [54.32, 'Строка']
+print_params(*values_list_2, 15)
+#print_params(b = 25) потеряны отсальные передаваемые параментры
+#print_params(c = [1,2,3]) потеряны отсальные передаваемые параментры
