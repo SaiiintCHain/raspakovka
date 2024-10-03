566ec72f906fe3a73196bd873069e1db539d9c81
SaiiintCHain <zok-hor@mail.ru>
2024-10-03 13:53:34 +0300

    Update raspakouka.py

--- raspakouka.py
+++ raspakouka.py
@@ -1,4 +1,4 @@
-def print_params(a, b ,c):
+def print_params(a = 3, b = 6 ,c = 5):
     print(a, b, c)
 
 values_list = [1, 'None', True]
@@ -7,5 +7,5 @@ print_params(*values_list)
 print_params(**values_dict)
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 15)
-#print_params(b = 25) потеряны отсальные передаваемые параментры
-#print_params(c = [1,2,3]) потеряны отсальные передаваемые параментры
+print_params(b = 25) 
+print_params(c = [1,2,3]) 
