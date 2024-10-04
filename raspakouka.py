7bc1755881bb32536e42c38874c066374ef8dccb
SaiiintCHain <zok-hor@mail.ru>
2024-10-04 18:33:21 +0300

    Update raspakouka.py

--- raspakouka.py
+++ raspakouka.py
@@ -1,11 +1,11 @@
-def print_params(a = 3, b = 6 ,c = 5):
+def print_params(a = 1, b = 'string' ,c = True):
     print(a, b, c)
 
-values_list = [1, 'None', True]
-values_dict = {'a': 1, 'b': 'None', 'c': True}
+values_list = [15, 'None', True]
+values_dict = {'a': 15, 'b': 'None', 'c': True}
 print_params(*values_list)
 print_params(**values_dict)
 values_list_2 = [54.32, 'Строка']
 print_params(*values_list_2, 15)
-print_params(b = 25) 
-print_params(c = [1,2,3]) 
+print_params(b = 25)
+print_params(c = [1,2,3])
